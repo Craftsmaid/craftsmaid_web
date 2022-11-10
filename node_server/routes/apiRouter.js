ad3ec3fc859a39dd35def5a257f8cf3fee29dee7
= <=>
2022-11-10 11:06:14 +0000

    uploaded all changes with controllers

--- node_server/routes/apiRouter.js
+++ node_server/routes/apiRouter.js
@@ -6,6 +6,21 @@ const {
 	updateMenu,
 	addNewFood,
 } = require("../controllers/menu.controller");
+const {
+	addPlan,
+	getPlan,
+	getPlansByWeek,
+	editPlan,
+	deletePlan,
+} = require("../controllers/plan.controller");
+const { addSchool } = require("../controllers/school.controller");
+const {
+	addWard,
+	editWard,
+	getWard,
+	getWards,
+	deleteWard,
+} = require("../controllers/ward.controller");
 const apiRouter = express.Router();
 
 apiRouter.get("/", async (req, res) => {
@@ -19,4 +34,22 @@ apiRouter.post("/getMenu", getMenu);
 apiRouter.post("/updateMenu", updateMenu);
 apiRouter.post("/addNewFood", addNewFood);
 
+// Plan endpoints
+apiRouter.post("/createPlan", addPlan);
+apiRouter.post("/getPlan", getPlan);
+apiRouter.post("/getPlansByWeek", getPlansByWeek);
+apiRouter.post("/editPlan", editPlan);
+apiRouter.delete("/delPlan", deletePlan);
+
+// Ward endpoints
+apiRouter.post("/createWard", addWard);
+apiRouter.post("/editWard", editWard);
+apiRouter.post("/getWard", getWard);
+apiRouter.post("/getWards", getWards);
+apiRouter.post("/deleteWard", deleteWard);
+
+// school
+apiRouter.post('/createSchool', addSchool);
+
+// 
 module.exports = apiRouter;
